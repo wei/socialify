3de220a5ef3059142161828989ff66f0bef3139e
Rahul Tarak <rahultarak12345@gmail.com>
2020-09-27 02:16:44 +0530

    ➖ Removed purgecss completely

--- config-overrides.js
+++ config-overrides.js
@@ -1,31 +1,3 @@
-const glob = require('glob-all')
-const paths = require('react-scripts/config/paths')
+const { override, addBabelPlugins } = require('customize-cra')
 
-const {
-  override,
-  addPostcssPlugins,
-  addBabelPlugins
-} = require('customize-cra')
-
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    paths.appHtml,
-    ...glob.sync(`${paths.appSrc}/**/*.ts*`, { nodir: true }),
-    ...glob.sync(`${paths.appNodeModules}/antd/es/**/*.css`, {
-      nodir: true
-    })
-  ],
-  extractors: [
-    {
-      extractor: content => content.match(/([a-zA-Z-]+)(?= {)/g) || [],
-      extensions: ['css']
-    }
-  ]
-})
-
-module.exports = override(
-  addPostcssPlugins([
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
-  ]),
-  addBabelPlugins('babel-plugin-relay')
-)
+module.exports = override(addBabelPlugins('babel-plugin-relay'))

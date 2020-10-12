2931cd9bfe113ee9243c656c4c3251a8da669f63
Wei He <git@weispot.com>
2020-10-12 16:32:22 -0400

    ⚡️ Reduce screenshotterroundtrip

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -1,40 +1,46 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import fetch from 'node-fetch'
+import renderCard from '../../common/renderCard'
+import QueryType from '../../common/types/queryType'
 
 const SCREENSHOT_ENDPOINT =
   process.env.SCREENSHOT_ENDPOINT || 'https://screenshotter.git.ci/screenshot'
-const DOMAIN = process.env.DOMAIN || 'https://socialify.git.ci'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const pathname = (req.url || '').split('?')[0]
-  const query = (req.url || '').split('?')[1] || ''
-  let [, owner, repo, filetype] = pathname.split('/')
-  if (filetype === 'jpg') {
+
+  let filetype = 'png'
+  if (pathname.includes('jpg')) {
     filetype = 'jpeg'
   }
 
-  const url = `${DOMAIN}/${owner}/${repo}/html?${query}`
-  const screenshotConfig: { [key: string]: string } = {
-    url,
-    filetype,
-    viewport: '2048,1024',
-    dpr: '2',
-    selector: '.card-wrapper'
-  }
-  const screenshotterUrl = `${SCREENSHOT_ENDPOINT}?${Object.entries(
-    screenshotConfig
-  )
-    .map(([k, v]) => `${k}=${encodeURIComponent(v)}`)
-    .join('&')}`
-
   try {
-    const response = await fetch(screenshotterUrl)
+    const url = `data:text/html;base64,${Buffer.from(
+      await renderCard(req.query as QueryType)
+    ).toString('base64')}`
+
+    const screenshotConfig: { [key: string]: string } = {
+      url,
+      filetype,
+      viewport: '2048,1024',
+      dpr: '2',
+      selector: '.card-wrapper'
+    }
+
+    const response = await fetch(SCREENSHOT_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(screenshotConfig)
+    })
     if (!response.ok) throw new Error('Request failed')
     const buffer = await response.buffer()
     res.setHeader('Content-Type', `image/${filetype}`)
     res.setHeader('Cache-Control', 'max-age=14400, public')
     res.status(200).send(buffer)
   } catch (error) {
+    console.error(error)
     res.status(422).send(error.message)
   }
 }

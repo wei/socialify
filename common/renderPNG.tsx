474fd3957f2b743dd6517000406b8aaa5424f997
Wei He <git@weispot.com>
2022-12-03 20:34:17 -0500

    ⚡ Generate png using resvg wasm, Remove @vercel/og

--- common/renderPNG.tsx
+++ common/renderPNG.tsx
@@ -1,21 +1,26 @@
-import { ImageResponse } from '@vercel/og'
-import Card from '../src/components/preview/card'
-import { getCardConfig, getFonts } from './renderCard'
+import * as resvg from '@resvg/resvg-wasm'
+// @ts-ignore
+import resvgWasm from '../public/resvg_bg.wasm?module'
+
+import renderCardSVG from './renderSVG'
 import QueryType from './types/queryType'
 
-const renderCardPNG = async (
-  query: QueryType,
-  opts: { headers?: Record<string, string> } = {}
-) => {
-  const config = await getCardConfig(query)
+const renderCardPNG = async (query: QueryType) => {
+  const [svg] = await Promise.all([
+    renderCardSVG(query),
+    resvg.initWasm(resvgWasm)
+  ])
 
-  return new ImageResponse(<Card {...config} />, {
-    width: 1280,
-    height: 640,
-    fonts: await getFonts(config.font),
-    emoji: 'twemoji',
-    ...opts
+  const resvgJS = new resvg.Resvg(svg, {
+    fitTo: {
+      mode: 'width',
+      value: 1280
+    }
   })
+  const pngData = resvgJS.render()
+  const pngBuffer = pngData.asPng()
+
+  return pngBuffer
 }
 
 export default renderCardPNG

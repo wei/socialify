01d5d585c14ae6821ff18ef3bf4f0b8f1382d9b4
Wei He <git@weispot.com>
2024-04-11 21:47:54 +0000

    🐛 Fix emojis
    Fixes #255

--- common/twemoji.ts
+++ common/twemoji.ts
@@ -33,7 +33,9 @@ function toCodePoint(unicodeSurrogates: string) {
 
 export const apis = {
   twemoji: (code: string) =>
-    'https://twemoji.maxcdn.com/v/latest/svg/' + code.toLowerCase() + '.svg',
+    'https://cdnjs.cloudflare.com/ajax/libs/twemoji/15.1.0/svg/' +
+    code.toLowerCase() +
+    '.svg',
   openmoji: 'https://cdn.jsdelivr.net/npm/@svgmoji/openmoji@2.0.0/svg/',
   blobmoji: 'https://cdn.jsdelivr.net/npm/@svgmoji/blob@2.0.0/svg/',
   noto: 'https://cdn.jsdelivr.net/gh/svgmoji/svgmoji/packages/svgmoji__noto/svg/',

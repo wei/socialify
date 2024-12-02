f255cb440748393481a32b2bc2c6486fcfe7627d
Keming He <keminghe.career@gmail.com>
2024-12-02 16:35:50 -0500

    💄 Proposed new landing page ui (DaisyUI v4)
    Updated landing page UI to be compatible with DaisyUI v4. Also proposing minor simplifications for best migration compatibility.

--- src/components/repo/repo.tsx
+++ src/components/repo/repo.tsx
@@ -33,24 +33,24 @@ const Repo: React.FC = () => {
     <main className="hero">
       <div className="hero-content">
         <div className="flex flex-col gap-6 max-w-xxl">
-          <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-secondary to-secondary-focus">
+          <h1 className="text-5xl font-extrabold text-secondary">
             Start with a <span className="inline-block">GitHub repo</span>
           </h1>
-          <div className="card w-full shadow-2xl bg-base-200">
+          <div className="card w-full shadow-2xl bg-base-300">
             <div className="card-body p-0">
               <form aria-labelledby="form-title" onSubmit={onSubmit}>
                 <div className="form-control">
-                  <div className="input-group">
+                  <div className="input-group flex items-center">
                     <label htmlFor="repo-input" className="sr-only">
                       GitHub Repository
                     </label>
-                    <span aria-hidden="true" className="pr-0 bg-base-200">
+                    <span aria-hidden="true" className="ps-4 flex-shrink-0">
                       <FiGithub className="w-6 h-6" />
                     </span>
                     <input
                       id="repo-input"
                       name="repo-input"
-                      className="input flex-1 pl-3 font-bold bg-base-200 focus:outline-none"
+                      className="input flex-grow bg-transparent font-bold focus:outline-none focus:border-none"
                       ref={repoInputRef}
                       type="text"
                       value={repoInput}
@@ -63,10 +63,10 @@ const Repo: React.FC = () => {
                     />
                     <button
                       aria-label="Submit GitHub repo input"
-                      className="btn btn-square btn-primary"
+                      className="btn btn-square btn-ghost flex-shrink-0"
                       type="submit"
                     >
-                      <FaArrowCircleRight className="h-6 w-6" />
+                      <FaArrowCircleRight className="h-6 w-6 text-primary" />
                     </button>
                   </div>
                 </div>

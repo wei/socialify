9c50e83038380338222f06647bb1425046631c1e
Keming He <keminghe.career@gmail.com>
2024-12-11 18:49:00 -0500

    ✅ Added 1% allowed buffer to UI test, kept user story test 100%.

--- .playwright/mainUIConsistency.spec.ts
+++ .playwright/mainUIConsistency.spec.ts
@@ -3,6 +3,9 @@ import { type Page, expect, test } from '@playwright/test'
 // Give navigation expects a generous timeout of 60 seconds.
 const customPageLoadTimeout = { timeout: 60000 }
 
+// As a known CI issue, allow max 1% deviation in pixel diff.
+const customDiffPixelRatio = { maxDiffPixelRatio: 0.01 }
+
 // Testing constants.
 const repoPreviewURL: string =
   '/wei/socialify?language=1&owner=1&name=1&stargazers=1&theme=Light'
@@ -18,7 +21,7 @@ test.describe('Socialify UI:', () => {
     await page.waitForTimeout(5000)
 
     const image = await page.screenshot()
-    expect(image).toMatchSnapshot()
+    expect(image).toMatchSnapshot(customDiffPixelRatio)
   })
 
   test('is consistent for error (404) page', async ({
@@ -28,7 +31,6 @@ test.describe('Socialify UI:', () => {
 
     // Wait for the page to load/hydrate completely.
     await page.waitForLoadState('networkidle', customPageLoadTimeout)
-    await page.waitForTimeout(5000)
 
     const image = await page.screenshot()
     expect(image).toMatchSnapshot()
@@ -48,10 +50,10 @@ test.describe('Socialify UI:', () => {
     await page.click('input[name="description"]')
 
     // Wait for the component transition/animation to finish completely.
-    await page.waitForTimeout(5000)
+    await page.waitForTimeout(1000)
 
     const image = await page.screenshot()
-    expect(image).toMatchSnapshot()
+    expect(image).toMatchSnapshot(customDiffPixelRatio)
 
     // Also check the toaster UI consistency.
     await page.click('button:has-text("URL")')
@@ -61,6 +63,6 @@ test.describe('Socialify UI:', () => {
     await page.waitForTimeout(1000)
 
     const toastImage = await page.screenshot()
-    expect(toastImage).toMatchSnapshot()
+    expect(toastImage).toMatchSnapshot(customDiffPixelRatio)
   })
 })

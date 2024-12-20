5ac566ce483712c8a3cb155954d406ed394f19b1
Wei He <git@weispot.com>
2024-12-20 15:38:28 -0500

    🐛 Fix unexpected page caching (#470)

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { JSX } from 'react'
 
 import Repo from '@/src/components/repo/repo'
 
+export const runtime = 'edge'
+
 export default function HomePage(): JSX.Element {
   return <Repo />
 }

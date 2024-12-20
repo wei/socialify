70b2fc3c7b7a0e2c4c91fedd7ad32cc541667d2d
Wei He <git@weispot.com>
2024-12-20 20:14:53 +0000

    🐛 Fix unexpected page caching

--- app/[_owner]/[_name]/page.tsx
+++ app/[_owner]/[_name]/page.tsx
@@ -4,6 +4,8 @@ import { JSX } from 'react'
 
 import MainRenderer from '@/src/components/mainRenderer'
 
+export const runtime = 'edge'
+
 export default function PreviewConfigPage(): JSX.Element {
   return <MainRenderer />
 }

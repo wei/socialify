2bff5aa3a86e3dcbc95cbd03ff2d24b7906a7758
Wei He <git@weispot.com>
2025-06-18 09:49:48 -0400

    Update index.ts

--- common/icons/index.ts
+++ common/icons/index.ts
@@ -1,5 +1,4 @@
-// Language icon mapping
-
 // Icon utility functions
 export { getSimpleIconsImageURI } from './iconUtils'
+// Language icon mapping
 export { LANGUAGE_ICON_MAPPING } from './languageMapping'
